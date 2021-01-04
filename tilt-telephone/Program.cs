091c6d1ba4bf0ed79aa9dc2793a1fb67ffbc74d5
Cody Sorgenfrey <cosorgen@microsoft.com>
2021-01-04 12:20:22 -0800

    Working read tilt, implement cloud loggin next

--- tilt-telephone/Program.cs
+++ tilt-telephone/Program.cs
@@ -1,132 +1,74 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Diagnostics;
 using Windows.Devices.Bluetooth.Advertisement;
-using Windows.Storage.Streams;
+using Beacons;
+using System.IO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Timers;
 
 namespace tilt_telephone
 {
-    public sealed class iBeaconData
+    class TiltSetting
     {
-        public Guid UUID { get; set; }
-        public ushort Major { get; set; }
-        public ushort Minor { get; set; }
-        public short TxPower { get; set; }
-        public double Distance { get; set; }
-        public short Rssi { get; set; }
-
-        public iBeaconData()
-        {
-            UUID = Guid.Empty;
-            Major = 0;
-            Minor = 0;
-            TxPower = 0;
-            Distance = 0;
-            Rssi = 0;
-        }
+        public string uuid;
+        public string color;
+        public float tempCali;
+        public float sgCali;
+        public string logginURL;
     }
-    static class Program
+    class Program
     {
-        static double CalculateDistance(int txPower, double rssi)
-        {
-            if (rssi == 0)
-            {
-                return -1.0; // if we cannot determine accuracy, return -1.
-            }
+        static private List<TiltSetting> tilts;
+        static private Timer timer;
+        static private int timeout = 10000;
+        static private bool watching;
+        static private iBeaconData curTilt;
 
-            double ratio = rssi * 1.0 / txPower;
-            if (ratio < 1.0)
-            {
-                return Math.Pow(ratio, 10);
-            }
-            else
-            {
-                double accuracy = (0.89976) * Math.Pow(ratio, 7.7095) + 0.111;
-                return accuracy;
-            }
-        }
-        public static void iBeaconSetAdvertisement(this BluetoothLEAdvertisement Advertisment, iBeaconData data)
-        {
-            BluetoothLEManufacturerData manufacturerData = new BluetoothLEManufacturerData();
-
-            // Set Apple as the manufacturer data
-            manufacturerData.CompanyId = 76;
-
-            var writer = new DataWriter();
-            writer.WriteUInt16(0x0215); //bytes 0 and 1 of the iBeacon advertisment indicator
-
-            if (data != null & data.UUID != Guid.Empty)
-            {
-                //If UUID is null scanning for all iBeacons
-                writer.WriteBytes(data.UUID.ToByteArray());
-                if (data.Major != 0)
-                {
-                    //If Major not null searching with UUID and Major
-                    writer.WriteBytes(BitConverter.GetBytes(data.Major).Reverse().ToArray());
-                    if (data.Minor != 0)
-                    {
-                        //If Minor not null we are looking for a specific beacon not a class of beacons
-                        writer.WriteBytes(BitConverter.GetBytes(data.Minor).Reverse().ToArray());
-                        if (data.TxPower != 0)
-                            writer.WriteBytes(BitConverter.GetBytes(data.TxPower));
-                    }
-                }
-            }
-
-            manufacturerData.Data = writer.DetachBuffer();
-
-            Advertisment.ManufacturerData.Clear();
-            Advertisment.ManufacturerData.Add(manufacturerData);
-        }
-        static iBeaconData iBeaconParseAdvertisement(this BluetoothLEAdvertisement Advertisment, short RawSignalStrengthInDBm)
-        {
-            iBeaconData beacon = null;
-            foreach (var adv in Advertisment.ManufacturerData)
-                if (adv.CompanyId == 76) //Apple
-                {
-                    var bytes = adv.Data.ToArray();
-                    if (bytes[0] == 0x02 && bytes[1] == 0x15 && bytes.Length == 23)
-                    {
-                        //iBeacon Data
-                        beacon = new iBeaconData();
-                        beacon.UUID = new Guid(bytes.Skip(2).Take(16).ToArray());
-                        beacon.Major = BitConverter.ToUInt16(bytes.Skip(18).Take(2).Reverse().ToArray(), 0);
-                        beacon.Minor = BitConverter.ToUInt16(bytes.Skip(20).Take(2).Reverse().ToArray(), 0);
-                        beacon.TxPower = (short)(sbyte)bytes[22];
-                        beacon.Rssi = RawSignalStrengthInDBm;
-
-                        //Estimated value
-                        //Read this article http://developer.radiusnetworks.com/2014/12/04/fundamentals-of-beacon-ranging.html 
-                        beacon.Distance = CalculateDistance(beacon.TxPower, RawSignalStrengthInDBm);
-
-                        Debug.WriteLine("UUID: " + beacon.UUID.ToString() + " Major: " + beacon.Major + " Minor:" + beacon.Minor + " Power: " + beacon.TxPower + " Rssi: " + RawSignalStrengthInDBm + " Distance:" + beacon.Distance);
-                    }
-                }
-
-            return beacon;
-        }
         static void Main(string[] args)
         {
-            var watcher = new BluetoothLEAdvertisementWatcher();
-            watcher.Received += OnAdvertisementRecieved;
-            watcher.AdvertisementFilter.Advertisement.iBeaconSetAdvertisement(new iBeaconData()/*{
-                UUID = Guid.Parse("{a495bb30-c5b1-4b44-b512-1370f02d74de}")
-            }*/);
-
-            Console.WriteLine("Looking for black tilt... (press Esc. to exit)");
-
-            watcher.Start();
-            while (Console.ReadKey().Key != ConsoleKey.Escape)
+            var json = File.ReadAllText("tilt-telephone.json");
+            tilts = JsonConvert.DeserializeObject<List<TiltSetting>>(json);
+            foreach (TiltSetting tilt in tilts)
             {
-            }
-            watcher.Stop();
+                var watcher = new BluetoothLEAdvertisementWatcher();
+                watcher.Received += OnAdvertisementRecieved;
+                watcher.AdvertisementFilter.Advertisement.iBeaconSetAdvertisement(new iBeaconData(){
+                    UUID = Guid.Parse(tilt.uuid)
+                });
 
-            Console.WriteLine("Stopped looking for black tilt.");
+                Console.Write("Looking for a {0} tilt", tilt.color);
+                timer = new Timer(timeout);
+                timer.Elapsed += OnTimerElapsed;
+                watching = true;
+                curTilt = null;
+
+                timer.Start();
+                watcher.Start();
+                while (watching)
+                {       
+                }
+                watcher.Stop();
+                timer.Stop();
+
+                Console.Write("\n");
+                if (curTilt != null)
+                {
+                    float gravity = (curTilt.Minor / 1000.00f) + tilt.sgCali;
+                    float temp = curTilt.Major + tilt.tempCali;
+                    Console.WriteLine("    Found a {0} tilt.", tilt.color);
+                    Console.WriteLine("    Logging to cloud...");
+                    Console.WriteLine("        Gravity: {0}", gravity);
+                    Console.WriteLine("        Temp: {0}", temp);
+                }
+                   
+            }
         }
+
+        private static void OnTimerElapsed(Object source, ElapsedEventArgs e)
+        {
+            watching = false;
+        }
+
         private static void OnAdvertisementRecieved(BluetoothLEAdvertisementWatcher watcher, BluetoothLEAdvertisementReceivedEventArgs eventArgs)
         {
             var beaconData = eventArgs.Advertisement.iBeaconParseAdvertisement(eventArgs.RawSignalStrengthInDBm);
@@ -134,7 +76,8 @@ namespace tilt_telephone
             if (beaconData == null)
                 return;
 
-            Console.WriteLine("{0}: {1}, {2}", beaconData.UUID, beaconData.Major, beaconData.Minor);
+            watching = false;
+            curTilt = beaconData;
         }
     }
 }
